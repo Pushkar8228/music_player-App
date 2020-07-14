9635eb20d60162f4c876a87af19858ebe1006f34
Pushkar8228 <56770867+Pushkar8228@users.noreply.github.com>
2020-07-14 23:53:11 +0530

    Music_Player App

---
+++ app/src/main/java/com/example/music_player/Main2Activity.java
@@ -0,0 +1,112 @@
+package com.example.music_player;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+
+import java.io.File;
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+public class Main2Activity extends AppCompatActivity {
+
+    String channelId="some_channel_id";
+    ListView listView;
+    String[] items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                        display();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        // check for permanent denial of permission
+                        if (response.isPermanentlyDenied()) {
+
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+
+
+    }
+
+    public ArrayList<File> findSong(File root){
+        ArrayList<File> at = new ArrayList<File>();
+        File[] files = root.listFiles();
+        for(File singleFile : files){
+            if(singleFile.isDirectory() && !singleFile.isHidden()){
+                at.addAll(findSong(singleFile));
+            }
+            else{
+                if(singleFile.getName().endsWith(".mp3") ||
+                        singleFile.getName().endsWith(".wav")){
+                    at.add(singleFile);
+                }
+            }
+        }
+        return at;
+    }
+    void display(){
+        final ArrayList<File> mySongs = findSong(Environment.getExternalStorageDirectory());
+        items = new String[ mySongs.size() ];
+        for(int i=0;i<mySongs.size();i++){
+            //toast(mySongs.get(i).getName().toString());
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
+        }
+        ArrayAdapter<String> adp = new
+                ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adp);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int
+                    position, long l) {
+
+                String songName = listView.getItemAtPosition(position).toString();
+                startActivity(new Intent(getApplicationContext(),PlayerActivity.class)
+
+                        .putExtra("pos",position).putExtra("songs",mySongs).putExtra("songname",songName));
+            }
+        });
+    }
+
+}
